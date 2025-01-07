c8e62f793cdbc0a32569d304bc7b63092aa8910e
Huu Dang Pham <danghuupham@usf.edu>
2025-01-07 17:27:01 +0700

    modified

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,34 +2,11 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "../components/Header";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Nội thất Hữu Đăng",
   description: "Trang trí nội thất Hữu Đăng",
-  icons: {
-    icon: [
-      {
-        url: "/images/logoplain.png",
-        sizes: "32x32",
-        type: "image/png",
-      },
-      {
-        url: "/images/logoplain.png",
-        sizes: "16x16",
-        type: "image/png",
-      },
-    ],
-    apple: [
-      {
-        url: "../images/logoplain.png",
-        sizes: "180x180",
-        type: "image/png",
-      },
-    ],
-    shortcut: ["../images/logoplain.png"],
-  },
 };
 
 export default function RootLayout({ children }) {
